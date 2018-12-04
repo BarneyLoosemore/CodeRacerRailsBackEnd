d9d7a08c9068e4540d8f78cac4de9ce328ae19d5
BarneyLoosemore <biscuitfull@gmail.com>
2018-12-04 19:42:53 +0000

    added ability to show one user

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,6 +2,15 @@ class Api::V1::GamesController < ApplicationController
 
     def create
         @game = Game.create(game_params)
+        @games = Game.all
+
+        gamesWithThisSnippet = @games.select{|game| game.snippet_id == @game.snippet_id }
+        
+        betterThanTheseGamesWithSnippet = gamesWithThisSnippet.select{|game| game.time_taken > @game.time_taken }
+        
+
+        @game.better_than_this_perc_people_snippet_time = 
+        @game.better_than_this_perc_people_chars_per_min = 
     end
 
     def index
--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,3 +1,3 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :snippet_id, :time_taken, :accuracy_percentage, :characters_per_min, :user, :snippet
+  attributes :id, :user_id, :snippet_id, :time_taken, :accuracy_percentage, :characters_per_min, :better_than_this_perc_people_snippet_time, :better_than_this_perc_people_chars_per_min, :user, :snippet
 end
