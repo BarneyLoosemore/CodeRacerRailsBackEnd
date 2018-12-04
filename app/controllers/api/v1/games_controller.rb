16a9f87fc0cfb12c0492e04f98e6cc9a0c809251
BarneyLoosemore <biscuitfull@gmail.com>
2018-12-04 20:09:14 +0000

    added show for games

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,16 +1,30 @@
 class Api::V1::GamesController < ApplicationController
 
     def create
-        @game = Game.create(game_params)
+        @game = Game.new(game_params)
         @games = Game.all
 
+        puts @game.characters_per_min
+
         gamesWithThisSnippet = @games.select{|game| game.snippet_id == @game.snippet_id }
-        
-        betterThanTheseGamesWithSnippet = gamesWithThisSnippet.select{|game| game.time_taken > @game.time_taken }
-        
+        betterThanTheseGamesWithSnippet = gamesWithThisSnippet.select{|game| game.time_taken < @game.time_taken }
 
         @game.better_than_this_perc_people_snippet_time = 
+            ((betterThanTheseGamesWithSnippet.length)*100 / (gamesWithThisSnippet.length-1)*100)/100
+
+        puts @games
+
+        fasterThanTheseGames = @games.select{|game| game.characters_per_min < @game.characters_per_min }
+
         @game.better_than_this_perc_people_chars_per_min = 
+            ((fasterThanTheseGames.length)*100 / (games.length-1)*100)/100
+
+        @game.save
+    end
+
+    def show
+        @game = Game.find(params[:id])
+        render json: @game
     end
 
     def index
