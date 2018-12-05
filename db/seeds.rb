15075fea58c751cf389532c76efe7bb0d529c11b
Amalie Smidth <amaliesmidth@gmail.com>
2018-12-05 13:30:25 +0000

    added new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,24 @@
 
 # snippet seeds 
 Snippet.create(content: "const hello = () => {\n  console.log('Hello World')\n}")
-Snippet.create(content: "const person = {\n name: 'Barney',\n age: 21,\n occupation: 'student'\n}")
-Snippet.create(content: "getData = () => {\n  fetch('data.json')\n  .then(resp => resp.json())\n  .then(data => console.log(data))\n}")
+Snippet.create(content: "const person = {\n  name: 'Barney',\n  age: 21,\n  occupation: 'student'\n}")
+Snippet.create(content: "getData = () => {\n  fetch('data.json')\n   .then(resp => resp.json())\n   .then(data => console.log(data))\n}")
 Snippet.create(content: "const array = [1, 2, 3, 4]\nconst four = array.length")
-Snippet.create(content: "function biggestNum(num1, num2){\n  return num1 > num2 ? num1 : num2\n}")
-Snippet.create(content: "const array = ['Dog', 'Cat', 'Sheep', 'Horse']\nconst dog = array.filter(animal => animal === 'Dog')")
+Snippet.create(content: "function biggestNum(num1, num2) {\n  return num1 > num2 ? num1 : num2\n}")
+Snippet.create(content: "const animals = ['Dog', 'Cat', 'Sheep', 'Horse', 'John']\nconst dog = animals.filter(animal => animal === 'Dog')")
 Snippet.create(content: "const nums = [14, 5, 9, 3, 11]\nconst sortedNums = nums.sort((a, b) => a - b)")
 Snippet.create(content: "const addTwo = (nums) => {\n  return nums.map(num => num + 2)\n}")
 Snippet.create(content: "const addNums = (num1, num2) => {\n  return num1 + num2\n}")
+Snippet.create(content: "getData = async url => {\n  const response = await fetch(url)\n  return response.json()\n}")
+Snippet.create(content: "createToy = async (toy, url) => {\n  const response = await fetch(url, {\n    method: 'POST',\n    headers: {\n      'Content-Type': 'application/json',\n      Accept: 'application/json'\n    },\n    body: JSON.stringify(toy)\n  })\n  return response.json()\n}")
+
+
+# Snippet.create(content: "const hello = () => {\n  console.log('Hello World')\n}")
+# Snippet.create(content: "const person = {\n name: 'Barney',\n age: 21,\n occupation: 'student'\n}")
+# Snippet.create(content: "getData = () => {\n  fetch('data.json')\n  .then(resp => resp.json())\n  .then(data => console.log(data))\n}")
+# Snippet.create(content: "const array = [1, 2, 3, 4]\nconst four = array.length")
+# Snippet.create(content: "function biggestNum(num1, num2){\n  return num1 > num2 ? num1 : num2\n}")
+# Snippet.create(content: "const array = ['Dog', 'Cat', 'Sheep', 'Horse']\nconst dog = array.filter(animal => animal === 'Dog')")
+# Snippet.create(content: "const nums = [14, 5, 9, 3, 11]\nconst sortedNums = nums.sort((a, b) => a - b)")
+# Snippet.create(content: "const addTwo = (nums) => {\n  return nums.map(num => num + 2)\n}")
+# Snippet.create(content: "const addNums = (num1, num2) => {\n  return num1 + num2\n}")
